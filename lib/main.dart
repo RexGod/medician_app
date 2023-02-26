68ff7d21689e6240fda901e1429c157c20a0eec7
RexGod <shadmehrmanoochehri@gmail.com>
2023-02-26 19:28:51 +0330

    change material app to getmaterial app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_2/screen_pages/homePage.dart';
+import 'package:get/get.dart';
+//import 'package:get_routes/routes/routes.dart';
 
 void main() {
   runApp(const MainApp());
@@ -11,11 +13,11 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // ignore: prefer_const_constructors
-    return MaterialApp(
-      initialRoute: '/homePage',
-      routes: {
-        homePage.routeName: (context) => homePage(),
-      },
+    return GetMaterialApp(
+      // ignore: prefer_const_literals_to_create_immutables
+      getPages: [
+        GetPage(name: 'homepage', page: () => homePage()),
+      ],
       theme: ThemeData(primaryColor: Color.fromRGBO(91, 154, 248, 0.498)),
       debugShowCheckedModeBanner: false,
     );
